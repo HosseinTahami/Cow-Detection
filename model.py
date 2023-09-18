3386504fdf09b8f87f45a7932e762c71bb653d40
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-18 11:33:32 +0330

    refactor: deleting extra files

--- model.py
+++ model.py
@@ -1,70 +1,46 @@
+import cv2
 import torch
-import torch.nn as nn
-import torch.optim as opt
-import torch.nn.functional as func
-from torch.utils.data import DataLoader
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+from google.colab.patches import cv2_imshow
 
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
+def detect_cows(model, video_path, output_path):
 
+    video = cv2.VideoCapture(video_path)
 
-class CNN(nn.Moudle):
-    def __init__(self, in_channels = 3, num_classes = 10):
-        super(CNN,self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=(3,3), stride=(1,1) padding=(1,1))
-        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1) padding(1,1))
-        self.fc1 = nn.Linear(16*7*7, num_classes)
-    
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
-        
-        return x
-    
+    fps = video.get(cv2.CAP_PROP_FPS)
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
-input_size = 784
-num_classes = 2
-learning_rate = 0.001
-batch_size = 64
-num_epochs = 1
+    while True:
+        ret, frame = video.read()
 
-#load data
-train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download = True)
-train_loader = DataLoader(dataset=train.dataset, batch_size= batch_size, shuffle = True)
-test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download = True)
-test_loader = DataLoader(dataset=train.dataset, batch_size= batch_size, shuffle = True)
+        if not ret:
+            break
 
-#initialize Model 
-model = CNN(input_size=input_size, num_classes=num_classes).todevice()
+        results = model(frame)
 
-# Loss & Optimizer
+        cows = results.pandas().xyxy[0][results.pandas().xyxy[0]['name'] == 'cow']
 
-criterion = nn.CrossEntropyLoss()
-Optimizer = optim.Adam(modle.parameters(), lr = learning_rate)
+        for _, cow in cows.iterrows():
+            xmin, ymin, xmax, ymax = cow[['xmin', 'ymin', 'xmax', 'ymax']]
+            cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 2)
 
-# Train network
+        out.write(frame)
 
-for epoch in range(num_epochs):
-    for batch_idx, (data, targets) in enumerate(train_loader)
+        cv2_imshow(frame)
 
-        data = data.to(device=device)
-        targets = targets.to(device=device)
-        
-        data = data.reshape(data.shape[0], -1)
-        
-        scores = model(data)
-        loss = criterion(scores, targets)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        
-        optimizer.step()
-        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video.release()
+    out.release()
+
+    cv2.destroyAllWindows()
+
+    print(f"Detection saved to {output_path}")
+
+detect_cows(model, 'first_video.mp4', 'output_video.mp4')
